63b586392cc47959e33290f860c7ef2099ec19eb
panopticonism <panopticonism@gmail.com>
2023-04-10 23:03:55 +0200

    Added axios package + projects fetch.

--- frontend/src/pages/Projects.tsx
+++ frontend/src/pages/Projects.tsx
@@ -1,6 +1,16 @@
+import axios from 'axios';
 import React from 'react';
 
 const Projects = () => {
+  const [data, setData] = React.useState(undefined);
+
+  React.useEffect(() => {
+    axios.get('/projects')
+      .then((res) => setData(res.data));
+  }, []);
+
+  console.log(data);
+
   return (
     <div>Projects</div>
   );
