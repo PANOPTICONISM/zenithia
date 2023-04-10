50f18cdaf02f071f4ade60b5ae6219d5f5c587f8
panopticonism <panopticonism@gmail.com>
2023-04-10 22:53:15 +0200

    Dotenv package to handle .env file on the backend.

--- backend/supabaseClient.js
+++ backend/supabaseClient.js
@@ -1,7 +1,9 @@
 import { createClient } from "@supabase/supabase-js";
+import * as dotenv from 'dotenv';
+dotenv.config()
 
 const supabaseUrl = "https://eaudflpgzewtokpbpvqi.supabase.co";
-const supabaseKey = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImVhdWRmbHBnemV3dG9rcGJwdnFpIiwicm9sZSI6ImFub24iLCJpYXQiOjE2ODExMjk3NjIsImV4cCI6MTk5NjcwNTc2Mn0.nQICGmh_K6BYPMt-OTyltG1PZJmt_6LgabLnxB6Iml4";
+const supabaseKey = process.env.SUPABASE_KEY;
 const supabase = createClient(supabaseUrl, supabaseKey);
 
 export default supabase;
