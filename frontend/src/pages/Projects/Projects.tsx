e2dbf08b17017b32623cf80a16f4fa57003110be
panopticonism <panopticonism@gmail.com>
2023-04-11 16:27:48 +0200

    Installed icons library + added delete button.

--- frontend/src/pages/Projects/Projects.tsx
+++ frontend/src/pages/Projects/Projects.tsx
@@ -1,29 +1,16 @@
-import axios from 'axios';
 import React from 'react';
 import { DataGrid } from '@mui/x-data-grid';
 import { Box } from '@mui/material';
-import { ProductProps } from './types';
-import { columns } from './Projects.utils';
+import { useColumnsAndRows } from './Projects.utils';
 
 const Projects = () => {
-  const [data, setData] = React.useState<ProductProps[] | undefined>(undefined);
-
-  React.useEffect(() => {
-    axios.get('/projects')
-      .then((res) => setData(res.data));
-  }, []);
-
-  console.log(data);
-
-  if (!data) {
-    return <>Is loading...</>;
-  }
+  const { columns, rows } = useColumnsAndRows();
 
   return (
     <Box sx={{ height: 400, width: '100%' }}>
       <DataGrid 
         columns={columns}
-        rows={data}
+        rows={rows}
       />
     </Box>
   );
