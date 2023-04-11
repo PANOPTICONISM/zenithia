021551b3da31099918d35ed3f04508d37d6e696b
panopticonism <panopticonism@gmail.com>
2023-04-11 19:39:31 +0200

    Fixed dates format.

--- frontend/src/pages/Projects/Projects.tsx
+++ frontend/src/pages/Projects/Projects.tsx
@@ -9,7 +9,13 @@ const Projects = () => {
   const { columns, rows } = useColumnsAndRows();
 
   const handleProcessRowUpdate = React.useCallback((newRow: ProductProps, oldRow: ProductProps) => {
-    axios.put(`/api/projects/${oldRow.id}`, newRow);
+    const fixDateFormat = (date: string) => {
+      return new Date(date).toLocaleDateString('en-US');
+    };
+
+    const row = {...newRow, start_date: fixDateFormat(newRow.start_date), finish_date: fixDateFormat(newRow.finish_date)};
+
+    axios.put(`/api/projects/${oldRow.id}`, row);
     return newRow;
   }, []);
 
