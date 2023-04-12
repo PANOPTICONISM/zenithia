b014a7b5e40984a0e8bfcdd51da4b99b3e56d35c
panopticonism <panopticonism@gmail.com>
2023-04-12 16:34:19 +0200

    Added button and title.

--- frontend/src/components/Header/Header.tsx
+++ frontend/src/components/Header/Header.tsx
@@ -1,8 +1,23 @@
+import { AddBoxOutlined } from '@mui/icons-material';
+import { Button } from '@mui/material';
 import React from 'react';
 
-const Header = () => {
+type HeaderProps = {
+    title: string;
+    handleClick: () => void;
+    buttonText: string;
+}
+
+const Header = ({ title, handleClick, buttonText }: HeaderProps) => {
   return (
-    <div>Header</div>
+    <header style={{ display: 'flex', justifyContent: 'space-between', alignItems: 'baseline' }}>
+      <h1 style={{ fontSize: '48px' }}>{title}</h1>
+      <Button 
+        sx={{ maxHeight: '37px', background: '#1F2C4F', fontWeight: 'bold' }}
+        variant='contained' 
+        onClick={handleClick} 
+        startIcon={<AddBoxOutlined />}>{buttonText}</Button>
+    </header>
   );
 };
 
