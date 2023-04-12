44c41b979ffba876383d01b6e36a9cfc1fdb9cc4
panopticonism <panopticonism@gmail.com>
2023-04-12 16:51:01 +0200

    Minor styling.

--- frontend/src/components/Header/Header.tsx
+++ frontend/src/components/Header/Header.tsx
@@ -1,5 +1,5 @@
 import { AddBoxOutlined } from '@mui/icons-material';
-import { Box, Button, TextField } from '@mui/material';
+import { Box, Button, Stack, TextField } from '@mui/material';
 import React from 'react';
 
 type HeaderProps = {
@@ -18,8 +18,9 @@ const Header = ({ title, handleClick, buttonText, searchValue, setSearchValue }:
   return (
     <header style={{ display: 'flex', justifyContent: 'space-between', alignItems: 'baseline' }}>
       <h1 style={{ fontSize: '48px' }}>{title}</h1>
-      <Box sx={{ maxHeight: '37px', }}>
+      <Stack direction="row" spacing={2}>
         <TextField
+          size='small'
           id="search"
           type="search"
           label="Search"
@@ -31,7 +32,7 @@ const Header = ({ title, handleClick, buttonText, searchValue, setSearchValue }:
           variant='contained' 
           onClick={handleClick} 
           startIcon={<AddBoxOutlined />}>{buttonText}</Button>
-      </Box>
+      </Stack>
     </header>
   );
 };
