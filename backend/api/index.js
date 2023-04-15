c4e999a691fb3ed54fb31f25654642f67688344a
panopticonism <panopticonism@gmail.com>
2023-04-16 01:41:00 +0200

    Leggo.

--- backend/api/index.js
+++ backend/api/index.js
@@ -15,8 +15,21 @@ app.use(
 
 app.get('/api', (req, res) => {
   const path = `/api/projects`;
-  res.setHeader('Content-Type', 'text/html');
-  res.setHeader('Cache-Control', 's-max-age=1, stale-while-revalidate');
+  //set header first to allow request or origin domain (value can be different)
+  res.setHeader('Access-Control-Allow-Origin', '*');
+  res.setHeader('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept');
+  res.setHeader('Access-Control-Allow-Credentials', true);
+  res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, PATCH, OPTIONS, DELETE');
+
+//---- other code
+
+//Preflight CORS handler
+  if(req.method === 'OPTIONS') {
+      return res.status(200).json(({
+          body: "OK"
+      }))
+  }
+
   res.end(`Hello! Go to item: <a href="${path}">${path}</a>`);
 })
 
--- backend/api/projects.js
+++ backend/api/projects.js
@@ -1,16 +1,6 @@
 import supabase from "../supabaseClient.js";
 
-const whitelist = [
-    '*'
-  ];
-
-export const getProjects = async (req, res, next) => {
-    res.setHeader('Access-Control-Allow-Origin', '*');
-    res.setHeader('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, PATCH, DELETE');
-    res.setHeader('Access-Control-Allow-Headers', 'X-Requested-With,Content-Type,Authorization');
-    res.setHeader('Access-Control-Allow-Credentials', true);
-  // Pass to next layer of middleware
-  if (!req.method === 'OPTIONS') next();
+export const getProjects = async (req, res) => {
     try {
         const { data, error } = await supabase.from('projects').select();
     
