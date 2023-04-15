cb1a7b318cd148b6b89cc6d9dfd6349868e1f860
panopticonism <panopticonism@gmail.com>
2023-04-16 00:45:12 +0200

    Standalone.

--- backend/api/index.js
+++ backend/api/index.js
@@ -22,8 +22,10 @@ app.use(
   })
 );
 
-app.get('/', (req, res) => {
-  res.end(`Hello!`);
+app.get('/api', (req, res) => {
+  res.setHeader('Content-Type', 'text/html');
+  res.setHeader('Cache-Control', 's-max-age=1, stale-while-revalidate');
+  res.end(`Hello! Go to item: <a href="${path}">${path}</a>`);
 })
 
 app.get('/api/projects', getProjects);
