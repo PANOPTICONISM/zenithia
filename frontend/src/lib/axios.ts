f393ecb658202f58be5c187656ae8bf6e3e93048
panopticonism <panopticonism@gmail.com>
2023-04-15 23:54:29 +0200

    Added axios headers.

--- frontend/src/lib/axios.ts
+++ frontend/src/lib/axios.ts
@@ -1,5 +1,9 @@
 import axios from 'axios';
 
 export const requester = axios.create({
-  baseURL: process.env.REACT_APP_API_URL
+  baseURL: process.env.REACT_APP_API_URL,
+  headers: {
+    'Access-Control-Allow-Origin': '*',
+    'Content-Type': 'application/json',
+  },
 });
