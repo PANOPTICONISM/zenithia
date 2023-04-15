c438c3b38e0f3a9386ca152e99c35bb236e7dd8c
panopticonism <panopticonism@gmail.com>
2023-04-16 00:55:53 +0200

    Error.

--- backend/api/index.js
+++ backend/api/index.js
@@ -14,6 +14,7 @@ app.use(
 );
 
 app.get('/', (req, res) => {
+  const path = `/api/item/${v4()}`;
   res.setHeader('Content-Type', 'text/html');
   res.setHeader('Cache-Control', 's-max-age=1, stale-while-revalidate');
   res.end(`Hello! Go to item: <a href="${path}">${path}</a>`);
