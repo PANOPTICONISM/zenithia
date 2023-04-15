802c48c51d7af1a0c98bd273aeb71c9831c19fdc
panopticonism <panopticonism@gmail.com>
2023-04-16 00:59:12 +0200

    Oi.

--- backend/index.js
+++ backend/index.js
@@ -2,7 +2,7 @@ import express from 'express';
 import pkg from 'body-parser';
 const app = express();
 const port = 4000;
-import { deleteProject, getProjects, postProject, updateProjects } from './projects.js';
+import { deleteProject, getProjects, postProject, updateProjects } from './api/projects.js';
 
 const { json, urlencoded } = pkg;
 
