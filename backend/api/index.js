c2fe557ef0f4f82b54a658c2cda5379fe36b86ae
panopticonism <panopticonism@gmail.com>
2023-04-16 01:22:19 +0200

    CORS.

--- backend/api/index.js
+++ backend/api/index.js
@@ -13,6 +13,20 @@ app.use(
   })
 );
 
+app.use((req, res, next) => {
+  const origin = req.get('referer');
+  const isWhitelisted = whitelist.find((w) => origin && origin.includes(w));
+  if (isWhitelisted) {
+    res.setHeader('Access-Control-Allow-Origin', '*');
+    res.setHeader('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, PATCH, DELETE');
+    res.setHeader('Access-Control-Allow-Headers', 'X-Requested-With,Content-Type,Authorization');
+    res.setHeader('Access-Control-Allow-Credentials', true);
+  }
+  // Pass to next layer of middleware
+  if (req.method === 'OPTIONS') res.sendStatus(200);
+  else next();
+});
+
 app.get('/api', (req, res) => {
   const path = `/api/projects`;
   res.setHeader('Content-Type', 'text/html');
