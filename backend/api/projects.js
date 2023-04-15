062a3e41d6cf537f301236419d954534843e5e45
panopticonism <panopticonism@gmail.com>
2023-04-16 01:29:25 +0200

    Update.

--- backend/api/projects.js
+++ backend/api/projects.js
@@ -5,17 +5,12 @@ const whitelist = [
   ];
 
 export const getProjects = async (req, res, next) => {
-    const origin = req.get('referer');
-  const isWhitelisted = whitelist.find((w) => origin && origin.includes(w));
-  if (isWhitelisted) {
     res.setHeader('Access-Control-Allow-Origin', '*');
     res.setHeader('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, PATCH, DELETE');
     res.setHeader('Access-Control-Allow-Headers', 'X-Requested-With,Content-Type,Authorization');
     res.setHeader('Access-Control-Allow-Credentials', true);
-  }
   // Pass to next layer of middleware
-  if (req.method === 'OPTIONS') res.sendStatus(200);
-  else next();
+  if (!req.method === 'OPTIONS') next();
     try {
         const { data, error } = await supabase.from('projects').select();
     
