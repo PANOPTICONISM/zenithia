fb279b5c6eb818150f5be05dc89b03b4f013e1c2
panopticonism <panopticonism@gmail.com>
2023-04-16 23:11:37 +0200

    Added mobile nav.

--- frontend/src/components/Sidebar/Sidebar.tsx
+++ frontend/src/components/Sidebar/Sidebar.tsx
@@ -6,90 +6,137 @@ import IconButton from '@mui/material/IconButton';
 import AllOutIcon from '@mui/icons-material/AllOut';
 import ArrowBackIosIcon from '@mui/icons-material/ArrowBackIos';
 import MenuIcon from '@mui/icons-material/Menu';
-import { AccountTreeOutlined, AnalyticsOutlined, CalendarMonthOutlined, GridViewOutlined, HourglassBottomOutlined, InsightsOutlined, ListAltOutlined, TrackChangesOutlined } from '@mui/icons-material';
-import { Drawer, DrawerHeader, ListLink, Subtitle } from './Sidebar.utils';
+import { AccountTreeOutlined, AnalyticsOutlined, ArrowForwardIos, CalendarMonthOutlined, GridViewOutlined, HourglassBottomOutlined, InsightsOutlined, ListAltOutlined, TrackChangesOutlined } from '@mui/icons-material';
+import { Drawer, DrawerHeader, ListLink, Subtitle, drawerWidth, drawerWidthMobile } from './Sidebar.utils';
 import { useIsSidebarOpen } from '../../contexts/SidebarProvider';
+import { AppBar, Stack, SwipeableDrawer, Toolbar, Typography, useMediaQuery } from '@mui/material';
+
+const linkIconsSecondGroup = (index: number) => {
+  if (index === 0) {
+    return <AccountTreeOutlined />;
+  } else if (index === 1) {
+    return <ListAltOutlined />;
+  }
+  return <HourglassBottomOutlined />;
+};
+
+const linkIconsThirdGroup = (index: number) => {
+  if (index === 0) {
+    return <TrackChangesOutlined />;
+  } else if (index === 1) {
+    return <AnalyticsOutlined />;
+  }
+  return <InsightsOutlined />;
+};
+
+const Navigation = ({ open } : { open: boolean; }) => {
+  return (
+    <>
+      <List disablePadding={open}>
+        <ListLink open={open} text="Dashboard" icon={<GridViewOutlined />} path="/" />
+      </List>
+      {open ? <Subtitle text='Pages' /> : <Divider />}
+      <List disablePadding={open}>
+        {['Calendar', 'Tasks'].map((text, index) => (
+          <ListLink 
+            key={index} 
+            path='/'
+            open={open} 
+            text={text} 
+            icon={index === 0 ? <CalendarMonthOutlined /> : <ListAltOutlined /> } />
+        ))}
+      </List>
+      {open ? <Subtitle text='Data' /> : <Divider />}
+      <List disablePadding={open}>
+        {['Projects', 'Clients', 'Hours'].map((text, index) => (
+          <ListLink 
+            key={index} 
+            path='/projects'
+            open={open} 
+            text={text} 
+            icon={linkIconsSecondGroup(index)} />
+        ))}
+      </List>
+      {open ? <Subtitle text='Performance' /> : <Divider />}
+      <List disablePadding={open}>
+        {['Revenue', 'Yearly', 'Monthly'].map((text, index) => (
+          <ListLink 
+            key={index}
+            path='/' 
+            open={open} 
+            text={text} 
+            icon={linkIconsThirdGroup(index)} />
+        ))}
+      </List>
+    </>
+  );
+};
 
 export default function Sidebar() {
+  const tabletBreakpoint = useMediaQuery('(max-width:900px)');
   const [open, setOpen] = useIsSidebarOpen();
-
-  const handleDrawerOpen = () => {
-    setOpen(true);
-  };
-
-  const handleDrawerClose = () => {
-    setOpen(false);
-  };
-
-  const linkIconsSecondGroup = (index: number) => {
-    if (index === 0) {
-      return <AccountTreeOutlined />;
-    } else if (index === 1) {
-      return <ListAltOutlined />;
-    }
-    return <HourglassBottomOutlined />;
-  };
-
-  const linkIconsThirdGroup = (index: number) => {
-    if (index === 0) {
-      return <TrackChangesOutlined />;
-    } else if (index === 1) {
-      return <AnalyticsOutlined />;
-    }
-    return <InsightsOutlined />;
-  };
 
   return (
     <Box>
-      <Drawer variant="permanent" open={open}>
-        <DrawerHeader>
-          <IconButton
-            color="inherit"
-            aria-label="open drawer"
-            onClick={open ? handleDrawerClose : handleDrawerOpen}
-            sx={{ marginTop: -3, right: open ? '-46px' : '-25px', position: 'absolute' }}
+      {/* {tabletBreakpoint ?
+        <>
+          <Stack direction="row" justifyContent="space-between" padding={2}>
+            <AllOutIcon sx={{ fontSize: '50px' }} />
+            <IconButton
+              color="inherit"
+              aria-label="open drawer"
+              onClick={() => setOpen(!open)}
+            >
+              {open ? <ArrowBackIosIcon /> : <MenuIcon />}
+            </IconButton>
+          </Stack>
+          <SwipeableDrawer open={open} onOpen={() => setOpen(true)} onClose={() => setOpen(false)}>
+            <Navigation open={open} />
+          </SwipeableDrawer>
+        </> :  */}
+      {tabletBreakpoint ? 
+        <>
+          <Stack direction="row" justifyContent="space-between" padding={2}>
+            <AllOutIcon sx={{ fontSize: '50px' }} />
+            <IconButton
+              color="inherit"
+              aria-label="open drawer"
+              onClick={() => setOpen(!open)}
+            >
+              {open ? <ArrowBackIosIcon /> : <MenuIcon />}
+            </IconButton>
+          </Stack>
+          <SwipeableDrawer open={open} onOpen={() => setOpen(true)} onClose={() => setOpen(false)}>
+            <Navigation open={open} />
+          </SwipeableDrawer>
+        </>
+        :
+        <>
+          <AppBar
+            position="fixed"
+            sx={{
+              width: `calc(100% - (60px + ${open ? drawerWidth : drawerWidthMobile}px))` ,
+              background: 'transparent',
+              boxShadow: 'none'
+            }}
           >
-            {open ? <ArrowBackIosIcon /> : <MenuIcon />}
-          </IconButton>
-          <AllOutIcon sx={{ fontSize: '50px', margin: '16px auto' }} />
-        </DrawerHeader>
-        <List disablePadding={open}>
-          <ListLink open={open} text="Dashboard" icon={<GridViewOutlined />} path="/" />
-        </List>
-        {open ? <Subtitle text='Pages' /> : <Divider />}
-        <List disablePadding={open}>
-          {['Calendar', 'Tasks'].map((text, index) => (
-            <ListLink 
-              key={index} 
-              path='/'
-              open={open} 
-              text={text} 
-              icon={index === 0 ? <CalendarMonthOutlined /> : <ListAltOutlined /> } />
-          ))}
-        </List>
-        {open ? <Subtitle text='Data' /> : <Divider />}
-        <List disablePadding={open}>
-          {['Projects', 'Clients', 'Hours'].map((text, index) => (
-            <ListLink 
-              key={index} 
-              path='/projects'
-              open={open} 
-              text={text} 
-              icon={linkIconsSecondGroup(index)} />
-          ))}
-        </List>
-        {open ? <Subtitle text='Performance' /> : <Divider />}
-        <List disablePadding={open}>
-          {['Revenue', 'Yearly', 'Monthly'].map((text, index) => (
-            <ListLink 
-              key={index}
-              path='/' 
-              open={open} 
-              text={text} 
-              icon={linkIconsThirdGroup(index)} />
-          ))}
-        </List>
-      </Drawer>
+            <Toolbar>
+              <IconButton
+                aria-label="open drawer"
+                edge="start"
+                onClick={() => setOpen(!open)}
+              >
+                {open ? <ArrowBackIosIcon /> : <ArrowForwardIos />}
+              </IconButton>
+            </Toolbar>
+          </AppBar>
+          <Drawer variant="permanent" open={open}>
+            <DrawerHeader>
+              <AllOutIcon sx={{ fontSize: '50px', margin: '16px auto' }} />
+            </DrawerHeader>
+            <Navigation open={open} />
+          </Drawer>
+        </>}
     </Box>
   );
 }
