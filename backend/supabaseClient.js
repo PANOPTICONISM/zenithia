36c85cbd968be9fb2c099330dd0ec2fefccf390a
panopticonism <panopticonism@gmail.com>
2023-04-16 02:30:43 +0200

    Backend conformed for deployment.

--- backend/supabaseClient.js
+++ backend/supabaseClient.js
@@ -2,7 +2,7 @@ import { createClient } from "@supabase/supabase-js";
 import * as dotenv from 'dotenv';
 dotenv.config()
 
-const supabaseUrl = "https://eaudflpgzewtokpbpvqi.supabase.co";
+const supabaseUrl = process.env.SUPABASE_URL;
 const supabaseKey = process.env.SUPABASE_KEY;
 const supabase = createClient(supabaseUrl, supabaseKey);
 
