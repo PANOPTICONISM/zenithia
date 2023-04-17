1af96eb333f06e42f273b5ba5a41d108c93575a5
panopticonism <panopticonism@gmail.com>
2023-04-17 21:45:24 +0200

    Colors added for mobile.

--- frontend/src/components/Header/Header.tsx
+++ frontend/src/components/Header/Header.tsx
@@ -3,7 +3,7 @@ import { Button, Stack } from '@mui/material';
 import React from 'react';
 import useMediaQuery from '@mui/material/useMediaQuery';
 import { SearchBar } from '../SearchBar/SearchBar';
-import { darkBlue } from '../../App';
+import { highlight } from '../../App';
 
 type HeaderProps = {
     title: string;
@@ -21,7 +21,7 @@ const Header = ({ title, handleClick, buttonText }: HeaderProps) => {
       <Stack direction={mobileBreakpoint ? 'column' : 'row'} spacing={2} paddingBottom="16px" justifyContent="flex-end">
         <SearchBar />
         <Button 
-          sx={{ background: darkBlue, fontWeight: 'bold' }}
+          sx={{ background: highlight, fontWeight: 'bold' }}
           variant='contained' 
           onClick={handleClick} 
           startIcon={<AddBoxOutlined />}>{buttonText}</Button>
