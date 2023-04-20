d6167653d595c5e97861b985f649ea70b791cfbc
panopticonism <panopticonism@gmail.com>
2023-04-20 18:11:46 +0200

    Routes + base page.

---
+++ frontend/src/pages/Tasks.tsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import Main from '../components/Main/Main';
+
+const Tasks = () => {
+  return (
+    <Main title="Tasks">
+      Tasks
+    </Main>
+  );
+};
+
+export default Tasks;
