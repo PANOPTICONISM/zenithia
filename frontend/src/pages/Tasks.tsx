73a6b62225f638d15bd707843dd90c429ddb0910
panopticonism <panopticonism@gmail.com>
2023-04-20 20:28:36 +0200

    Base components.

--- frontend/src/pages/Tasks.tsx
+++ frontend/src/pages/Tasks.tsx
@@ -1,20 +1,21 @@
 import React from 'react';
 import Main from '../components/Main/Main';
 import { DragDropContext, Droppable, Draggable, DropResult } from '@hello-pangea/dnd';
+import { Typography } from '@mui/material';
+import Column from '../components/KanbanColumn/KanbanColumn';
 
 type InitialProps = {
   tasks: {
     [key: string]: {
       id: string,
-      name: string,
-      thumb: string
+      content: string
     }
   },
   columns: {
     [key: string]: {
       id: string,
       title: string,
-      tasksId: string[]
+      taskIds: string[]
     }
   }
   columnOrder: string[]
@@ -22,53 +23,28 @@ type InitialProps = {
 
 const initialData: InitialProps = {
   tasks: {
-    'task-1': {
-      id: 'task-1',
-      name: 'Gary Goodspeed',
-      thumb: 'https://upload.wikimedia.org/wikipedia/commons/thumb/3/3a/Cat03.jpg/1200px-Cat03.jpg'
-    },
-    'task-2': {
-      id: 'task-2',
-      name: 'Little Cato',
-      thumb: 'https://upload.wikimedia.org/wikipedia/commons/thumb/3/3a/Cat03.jpg/1200px-Cat03.jpg'
-    },
-    'task-3': {
-      id: 'task-3',
-      name: 'KVN',
-      thumb: 'https://upload.wikimedia.org/wikipedia/commons/thumb/3/3a/Cat03.jpg/1200px-Cat03.jpg'
-    },
-    'task-4': {
-      id: 'task-4',
-      name: 'Quinn Ergon',
-      thumb: 'https://upload.wikimedia.org/wikipedia/commons/thumb/3/3a/Cat03.jpg/1200px-Cat03.jpg'
-    }
+    'task-1': { id: 'task-1', content: 'Take out the garbage' },
+    'task-2': { id: 'task-2', content: 'Watch my favorite show' },
+    'task-3': { id: 'task-3', content: 'Charge my phone' },
+    'task-4': { id: 'task-4', content: 'Cook dinner' },
   },
   columns: {
     'column-1': {
       id: 'column-1',
       title: 'To do',
-      tasksId: ['task-1', 'task-2', 'task-3', 'task-4']
-    }
+      taskIds: ['task-1', 'task-2', 'task-3', 'task-4'],
+    },
   },
-  columnOrder: ['column-1']
-};
-
-const Column = ({ column, tasks } : {column: {title: string}, tasks: any}) => {
-
-  return (
-    <div>
-      {column.title}
-    </div>
-  );
+  columnOrder: ['column-1'],
 };
 
 const Tasks = () => {
   const column = initialData.columns['column-1'];
-  const tasks = column.tasksId.map((taskId) => console.log(initialData.tasks[taskId]));
+  const tasks = column.taskIds.map(taskId => initialData.tasks[taskId]);
   
   return (
     <Main title="Tasks">
-      {initialData.columnOrder.map((order: string) => {
+      {initialData.columnOrder.map(() => {
         return <Column key={column.id} column={column} tasks={tasks} />;
       })}
     </Main>
