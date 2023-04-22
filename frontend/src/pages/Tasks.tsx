d6c5f58f9b031f3e4d1957efc957c534c85739af
panopticonism <panopticonism@gmail.com>
2023-04-22 22:21:55 +0200

    Draggable to other areas.

--- frontend/src/pages/Tasks.tsx
+++ frontend/src/pages/Tasks.tsx
@@ -29,9 +29,33 @@ const Tasks = () => {
 
   const handleDragEnd = (result: DropResult) => {
     if (!result.destination) return;
-    const { destination, draggableId } = result;
-    // updateTask(draggableId, { column_id: destination.droppableId })
-    //   .catch((error) => console.log('Update single field: ' + error));
+    const { source, destination, draggableId } = result;
+
+    if (source.droppableId !== destination.droppableId) {
+      const sourceColumn = columns.filter((column) => column.id === source.droppableId);
+      const destColumn = columns.filter((column) => column.id === destination.droppableId);
+      const sourceItems = [...sourceColumn][0].items;
+      const destItems = [...destColumn][0].items;
+      const [removed] = sourceItems.splice(source.index, 1);
+      destItems.splice(destination.index, 0, removed);
+
+      const removedSourceColumn = columns.filter((column) => column.id !== sourceColumn[0].id);
+      const removedDestColumn = removedSourceColumn.filter((column) => column.id !== destColumn[0].id);
+      const joinColumnChanges = [sourceColumn, destColumn, removedDestColumn].flat();
+      
+      updateTask(draggableId, { column_id: destination.droppableId })
+        .then(() => setColumns(joinColumnChanges.sort((a, b) => a.orderBy - b.orderBy)))
+        .catch((error) => console.log('Update single field: ' + error));
+    } else {
+      const sourceColumn = columns.filter((column) => column.id == source.droppableId);
+      const copiedItems = [...sourceColumn][0].items;
+      const [removed] = copiedItems.splice(source.index, 1);
+      copiedItems.splice(destination.index, 0, removed);
+      
+      const removedSourceColumn = columns.filter((column) => column.id !== sourceColumn[0].id);
+      const joinColumnChanges = [sourceColumn, removedSourceColumn].flat();
+      setColumns(joinColumnChanges.sort((a, b) => a.orderBy - b.orderBy));
+    }
     
   };
 
