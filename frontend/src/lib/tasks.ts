09f84442b2a553938ab93d0a24b30a8b128a08cd
panopticonism <panopticonism@gmail.com>
2023-04-22 19:04:53 +0200

    Retrieve tasks data from database.

---
+++ frontend/src/lib/tasks.ts
@@ -0,0 +1,43 @@
+import { AxiosError } from 'axios';
+import { requester } from './axios';
+
+type ServerError = { message: string; }
+
+export type TaskProps = {
+  id: string;
+  title: string;
+  importance: string,
+  project_id: string,
+  column_id: string,
+  deadline: string | null,
+}
+
+export type ColumnProps = {
+  id: string,
+  title: string;
+  orderBy: number,
+};
+
+export const getTasksColumns = async (): Promise<ColumnProps[]> => {
+  const path = '/api/tasks';
+
+  try {
+    const response = await requester.get(path);
+    return response.data as ColumnProps[];
+  } catch (err) {
+    const error = err as AxiosError<ServerError>;
+    throw new Error(error.response?.data?.message || error.message);
+  }
+};
+
+export const getTasks = async (): Promise<TaskProps[]> => {
+  const path = '/api/tasks/all';
+
+  try {
+    const response = await requester.get(path);
+    return response.data as TaskProps[];
+  } catch (err) {
+    const error = err as AxiosError<ServerError>;
+    throw new Error(error.response?.data?.message || error.message);
+  }
+};
