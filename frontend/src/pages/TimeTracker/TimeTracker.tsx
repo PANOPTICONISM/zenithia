30c93755681fcbaeed1373b34d6cc4fc67a49693
panopticonism <panopticonism@gmail.com>
2023-04-26 12:33:36 +0200

    Table + fetching data.

--- frontend/src/pages/TimeTracker/TimeTracker.tsx
+++ frontend/src/pages/TimeTracker/TimeTracker.tsx
@@ -1,12 +1,31 @@
 import React from 'react';
 import Main from '../../components/Main/Main';
+import { useColumnsAndRows } from './TimeTracker.utils';
+import { Box } from '@mui/material';
+import { DataGrid } from '@mui/x-data-grid';
+import { useFiltering } from '../../components/SearchBar/SearchBar.utils';
 
 export const TimeTracker = () => {
+  const { columns, rows, setRows } = useColumnsAndRows();
+  const { filterModel, handleFilterChange } = useFiltering({ columnField: 'title' });
+
   const addTracking = () => {
     console.log('add');
   };
   
   return (
-    <Main title="TimeTracker" handleClick={addTracking} buttonText='Add hours'>Hello</Main>
+    <Main title="TimeTracker" handleClick={addTracking} buttonText='Add hours'>
+      <Box sx={{ height: 400, width: '100%' }}>
+        <DataGrid 
+          columns={columns}
+          rows={rows}
+          editMode="row"
+          //   processRowUpdate={handleProcessRowUpdate}
+          //   onProcessRowUpdateError={handleProcessRowUpdateError}
+          filterModel={filterModel}
+          onFilterModelChange={handleFilterChange}
+        />
+      </Box>
+    </Main>
   );
 };
