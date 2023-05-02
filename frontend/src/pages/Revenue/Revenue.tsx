8c48c3be95676d1ff9fffb9f2afe8c0bedf986a9
panopticonism <panopticonism@gmail.com>
2023-05-02 18:28:42 +0200

    Earnings highlights styling.

--- frontend/src/pages/Revenue/Revenue.tsx
+++ frontend/src/pages/Revenue/Revenue.tsx
@@ -1,39 +1,38 @@
-import { Box } from '@mui/material';
+import { Box, Stack, Typography } from '@mui/material';
 import { DataGrid } from '@mui/x-data-grid';
 import Main from 'components/Main/Main';
 import React from 'react';
 import { useColumnsAndRows } from './Revenue.utils';
+import PublicIcon from '@mui/icons-material/Public';
+import TodayIcon from '@mui/icons-material/Today';
+import { grey } from 'App';
+
+const EarningsTimeline = ({ text, total, icon } : {text: string, total: string, icon: React.ReactNode}) => {
+
+  return (
+    <Stack direction="row" spacing={1} padding="24px" sx={{ border: `1px solid ${grey}`, width: '100%' }} alignItems="center">
+      {icon}
+      <Box>
+        <Typography fontWeight={700}>{text}</Typography>
+        <Typography fontSize="30px">{total}</Typography>
+      </Box>
+    </Stack>
+  );
+};
 
 const Revenue = () => {
   const { columns, data } = useColumnsAndRows();
 
-  //   const handleProcessRowUpdate = React.useCallback((newRow: ProjectProps, oldRow: ProjectProps) => {
-  //     if (!oldRow.id) {
-  //       return oldRow;
-  //     }
-    
-  //     const fixDateFormat = (date: string) => {
-  //       return new Date(date).toLocaleDateString('en-US');
-  //     };
-    
-  //     const row = { ...newRow, start_date: fixDateFormat(newRow.start_date), finish_date: fixDateFormat(newRow.finish_date) };
-    
-  //     // updateProject(oldRow.id, row).catch((error) => console.log('UPDATE: ' + error));
-  //     return newRow;
-  //   }, []);
-    
-  const handleProcessRowUpdateError = React.useCallback((error: Error) => {
-    console.log(error);
-  }, []);
-
   return (
     <Main title="Revenue">
+      <Stack direction="row" spacing={2} paddingBottom="24px">
+        <EarningsTimeline text="Earnings this year" total="0" icon={<PublicIcon sx={{ height: '100%', width: '70px' }} />} />
+        <EarningsTimeline text="Earnings this month" total="0" icon={<TodayIcon sx={{ height: '100%', width: '70px' }} />} />
+      </Stack>
       <Box sx={{ height: 400, width: '100%' }}>
         <DataGrid 
           columns={columns}
           rows={data}
-        //   processRowUpdate={handleProcessRowUpdate}
-        //   onProcessRowUpdateError={handleProcessRowUpdateError}
         />
       </Box>
     </Main>
