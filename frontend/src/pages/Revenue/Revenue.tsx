0a46060663f3caf083b03d329d361bd74fab5b07
panopticonism <panopticonism@gmail.com>
2023-05-02 19:01:12 +0200

    DataGrid informational boxes.

--- frontend/src/pages/Revenue/Revenue.tsx
+++ frontend/src/pages/Revenue/Revenue.tsx
@@ -8,14 +8,39 @@ import TodayIcon from '@mui/icons-material/Today';
 import { grey } from 'App';
 import { DateTime } from 'luxon';
 import { valueFormatter } from 'pages/Projects/Projects.utils';
+import EmojiObjectsIcon from '@mui/icons-material/EmojiObjects';
+import SavingsIcon from '@mui/icons-material/Savings';
 
 const EarningsTimeline = ({ text, total, icon } : {text: string, total: string, icon: React.ReactNode}) => {
-
   return (
     <Stack direction="row" spacing={1} padding="24px" sx={{ border: `1px solid ${grey}`, width: '100%' }} alignItems="center">
       {icon}
       <Box>
         <Typography fontWeight={700}>{text}</Typography>
+        <Typography fontSize="30px">{total}</Typography>
+      </Box>
+    </Stack>
+  );
+};
+
+const SuggestionBox = ({ title, tagline, icon } : {title: string, tagline: string, icon: React.ReactNode}) => {
+  return (
+    <Stack direction="row" spacing={1} padding="24px" sx={{ border: `1px solid ${grey}`, width: '100%' }} alignItems="center">
+      {icon}
+      <Box>
+        <Typography fontWeight={700}>{title}</Typography>
+        <Typography>{tagline}</Typography>
+      </Box>
+    </Stack>
+  );
+};
+
+const DataGridInfo = ({ text, total, icon } : {text: string, total: string, icon: React.ReactNode}) => {
+  return (
+    <Stack direction="row" spacing={1} padding="24px" sx={{ border: `1px solid ${grey}`, width: '100%' }} alignItems="center">
+      {icon}
+      <Box>
+        <Typography>{text}</Typography>
         <Typography fontSize="30px">{total}</Typography>
       </Box>
     </Stack>
@@ -38,6 +63,16 @@ const Revenue = () => {
       <Stack direction="row" spacing={2} paddingBottom="24px">
         <EarningsTimeline text="Earnings this year" total={valueFormatter.format(Number(totalYearlySum))} icon={<PublicIcon sx={{ height: '100%', width: '70px' }} />} />
         <EarningsTimeline text="Earnings this month" total={valueFormatter.format(Number(totalMonthlySum))} icon={<TodayIcon sx={{ height: '100%', width: '70px' }} />} />
+      </Stack>
+      <Stack direction="row" spacing={2} paddingBottom="24px">
+        <SuggestionBox 
+          title="Earnings as shown below" 
+          tagline="Add date filters in the table below to find out how much you earned in a specific timeframe." 
+          icon={<EmojiObjectsIcon sx={{ height: '100%', width: '70px' }} />} />
+        <EarningsTimeline 
+          text="Earnings this month" 
+          total={valueFormatter.format(Number(totalMonthlySum))} 
+          icon={<SavingsIcon sx={{ height: '100%', width: '70px' }} />} />
       </Stack>
       <Box sx={{ height: 400, width: '100%' }}>
         <DataGrid 
