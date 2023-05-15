921b052d937d0f2c2ac01b212a6fba02acdf5bf8
panopticonism <panopticonism@gmail.com>
2023-05-15 17:03:16 +0200

    Incorporated real data.

--- frontend/src/pages/BarChart/YearlyStats.tsx
+++ frontend/src/pages/BarChart/YearlyStats.tsx
@@ -3,27 +3,108 @@ import React from 'react';
 import { ResponsiveLine } from '@nivo/line';
 import { darkBlue } from 'App';
 import { Box } from '@mui/material';
+import { getProjects } from 'lib/projects';
+import { DateTime, Duration } from 'luxon';
+import { ProjectFormattedProps } from 'pages/Revenue/types';
+
+const useRevenueData = () => {
+  const [data, setData] = React.useState<ProjectFormattedProps[]>([]);
+
+  React.useEffect(() => {
+    getProjects('*, time_tracker(*), clients(*)')
+      .then((res) => {
+        const result = res.map((entry) => {
+          const initialValue = 0;
+          const hoursTotal = entry?.time_tracker?.reduce(
+            (accumulator, currentValue) => accumulator + (currentValue?.total || 0),
+            initialValue
+          );
+      
+          const totalMinutes = hoursTotal && Duration.fromMillis(hoursTotal).toFormat('mm');
+          let totalPrice = 0;
+          if (entry.base_price && hoursTotal) {
+            if (entry.revenue === 'Project') {
+              totalPrice = entry.base_price;
+            } else {
+              totalPrice = (entry.base_price / 100) * Number(totalMinutes);
+            }
+          }
+      
+          const obj = {
+            id: entry.id,
+            title: entry.title,
+            client: entry.clients?.name,
+            start_date: entry.start_date,
+            finish_date: entry.finish_date,
+            base_price: entry.base_price,
+            revenue: entry.revenue,
+            total: hoursTotal,
+            price_total: totalPrice,
+            time_tracker: entry.time_tracker,
+          };
+          return obj;
+        });
+        setData(result);
+      })
+      .catch((error) => console.log('GET: ' + error));
+  }, []);
+
+  const currentYear = new Date().getFullYear();
+  const yearlyLogs = React.useMemo(() => {
+    return data.map((entry) => {
+      const entries = entry?.time_tracker?.filter((time) => DateTime.fromFormat(time.date, 'yyyy-MM-dd').year === currentYear);
+      return { ...entry, time_tracker: entries };
+    });
+  }, [data]);
+
+  const currentMonth = new Date().getMonth();
+  const monthlyLogs = React.useMemo(() => {
+    return data.map((entry) => {
+      const entries = entry?.time_tracker?.filter((time) => DateTime.fromFormat(time.date, 'yyyy-MM-dd').month === currentMonth + 1);
+      return { ...entry, time_tracker: entries };
+    });
+  }, [data]);
+
+  return { data, setData, yearlyLogs, monthlyLogs };
+};
 
 const YearlyStats = () => {
-  const [data, setData] = React.useState([]);
+  const { data, yearlyLogs } = useRevenueData();
 
   const [total] = React.useMemo(() => {
     if (!data) {
       return [];
     }
 
-    const total = { id: 'yearlyRevenue', color: darkBlue, data: [{
-      'x': 'plane',
-      'y': 63
-    },
-    {
-      'x': 'helicopter',
-      'y': 293
-    },] };
+    const groupedByLogs = yearlyLogs.reduce(
+      (entryMap, e) => 
+        entryMap.set(DateTime.fromISO(e.finish_date).monthLong, 
+          [...entryMap.get(DateTime.fromISO(e.finish_date).monthLong)||[], e]),
+      new Map()
+    );
+
+    const totalData: {x: string | null, y: number | null}[] = [];
+    groupedByLogs.forEach((value, key, map) => {
+      const initialValue = 0;
+      const total = value.reduce(
+        (accumulator: number, currentValue: { total: number; }) => accumulator + (currentValue?.total || 0),
+        initialValue
+      );
+      const obj = {
+        x: key,
+        y: total
+      };
+      totalData.push(obj);
+    });
+
+    const total = { 
+      id: 'yearlyRevenue', 
+      color: darkBlue, 
+      data: totalData
+    };
+
     return [[total]];
   }, [data]);
-
-  console.log(total);
 
   if (!total) {
     return <>Is Loading...</>;
@@ -31,7 +112,7 @@ const YearlyStats = () => {
 
   return (
     <Main title='Bar chart'>
-      <Box height="500px">
+      <Box height="600px">
         <ResponsiveLine
           data={total}
           margin={{ top: 50, right: 110, bottom: 50, left: 60 }}
@@ -50,7 +131,7 @@ const YearlyStats = () => {
             tickSize: 5,
             tickPadding: 5,
             tickRotation: 0,
-            legend: 'transportation',
+            legend: 'Yearly Revenue Insights',
             legendOffset: 36,
             legendPosition: 'middle'
           }}
