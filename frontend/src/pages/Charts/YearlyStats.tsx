99a24154ef893d92b9960fcc3fb8def72e826315
panopticonism <panopticonism@gmail.com>
2023-05-15 19:23:08 +0200

    Code modularized.

--- frontend/src/pages/Charts/YearlyStats.tsx
+++ frontend/src/pages/Charts/YearlyStats.tsx
@@ -1,9 +1,9 @@
 import Main from 'components/Main/Main';
 import React from 'react';
-import { ResponsivePie } from '@nivo/pie';
 import { DateTime } from 'luxon';
 import { Box } from '@mui/material';
 import { useGetProjectsFormatted } from 'hooks/useGetProjectsFormatted';
+import PieChart, { PieChartProps } from 'components/Charts/PieChart';
 
 const useRevenueData = () => {
   const { projects, setProjects } = useGetProjectsFormatted();
@@ -19,7 +19,7 @@ const useRevenueData = () => {
     new Map()
   );
 
-  const yearlyLogsTotals: {id: string, label: string, value: number, color: string}[] = [];
+  const yearlyLogsTotals: PieChartProps = [];
   groupedByLogs.forEach((value, key) => {
     const initialValue = 0;
     const total = value.reduce(
@@ -44,133 +44,7 @@ const YearlyStats = () => {
   return (
     <Main title='Yearly Revenue'>
       <Box height="500px">
-        <ResponsivePie
-          data={yearlyLogsTotals}
-          margin={{ top: 40, right: 80, bottom: 80, left: 80 }}
-          innerRadius={0.5}
-          padAngle={0.7}
-          cornerRadius={3}
-          activeOuterRadiusOffset={8}
-          borderWidth={1}
-          borderColor={{
-            from: 'color',
-            modifiers: [
-              [
-                'darker',
-                0.2
-              ]
-            ]
-          }}
-          arcLinkLabelsSkipAngle={10}
-          arcLinkLabelsTextColor="#333333"
-          arcLinkLabelsThickness={2}
-          arcLinkLabelsColor={{ from: 'color' }}
-          arcLabelsSkipAngle={10}
-          arcLabelsTextColor={{
-            from: 'color',
-            modifiers: [
-              [
-                'darker',
-                2
-              ]
-            ]
-          }}
-          defs={[
-            {
-              id: 'dots',
-              type: 'patternDots',
-              background: 'inherit',
-              color: 'rgba(255, 255, 255, 0.3)',
-              size: 4,
-              padding: 1,
-              stagger: true
-            },
-            {
-              id: 'lines',
-              type: 'patternLines',
-              background: 'inherit',
-              color: 'rgba(255, 255, 255, 0.3)',
-              rotation: -45,
-              lineWidth: 6,
-              spacing: 10
-            }
-          ]}
-          fill={[
-            {
-              match: {
-                id: 'ruby'
-              },
-              id: 'dots'
-            },
-            {
-              match: {
-                id: 'c'
-              },
-              id: 'dots'
-            },
-            {
-              match: {
-                id: 'go'
-              },
-              id: 'dots'
-            },
-            {
-              match: {
-                id: 'python'
-              },
-              id: 'dots'
-            },
-            {
-              match: {
-                id: 'scala'
-              },
-              id: 'lines'
-            },
-            {
-              match: {
-                id: 'lisp'
-              },
-              id: 'lines'
-            },
-            {
-              match: {
-                id: 'elixir'
-              },
-              id: 'lines'
-            },
-            {
-              match: {
-                id: 'javascript'
-              },
-              id: 'lines'
-            }
-          ]}
-          legends={[
-            {
-              anchor: 'bottom',
-              direction: 'row',
-              justify: false,
-              translateX: 0,
-              translateY: 56,
-              itemsSpacing: 0,
-              itemWidth: 100,
-              itemHeight: 18,
-              itemTextColor: '#999',
-              itemDirection: 'left-to-right',
-              itemOpacity: 1,
-              symbolSize: 18,
-              symbolShape: 'circle',
-              effects: [
-                {
-                  on: 'hover',
-                  style: {
-                    itemTextColor: '#000'
-                  }
-                }
-              ]
-            }
-          ]}
-        />
+        <PieChart logs={yearlyLogsTotals} />
       </Box>
     </Main>
   );
