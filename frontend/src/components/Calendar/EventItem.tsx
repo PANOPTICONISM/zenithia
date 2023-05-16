e93b1f9a1f9cb8011b08d219b231309a8c70fa59
panopticonism <panopticonism@gmail.com>
2023-05-16 23:16:02 +0200

    Divided into multiple files.

---
+++ frontend/src/components/Calendar/EventItem.tsx
@@ -0,0 +1,14 @@
+import { EventContentArg } from '@fullcalendar/core';
+import { Box, Typography } from '@mui/material';
+import React from 'react';
+
+export const EventItem = ({ info }: { info: EventContentArg }) => {
+  const { event, timeText } = info;
+  
+  return (
+    <Box px="5px">
+      <Typography variant='overline' fontWeight={700}>{event.title}</Typography>
+      <Typography>{timeText}</Typography>
+    </Box>
+  );
+};
