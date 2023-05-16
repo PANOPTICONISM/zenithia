b54e5c2fd6c4cb0132a808e56204867fb494767f
panopticonism <panopticonism@gmail.com>
2023-05-16 16:51:26 +0200

    Base page + routing.

---
+++ frontend/src/pages/Schedule/Schedule.tsx
@@ -0,0 +1,13 @@
+import Calendar from 'components/Calendar/Calendar';
+import Main from 'components/Main/Main';
+import React from 'react';
+
+const Schedule = () => {
+  return (
+    <Main title="Calendar">
+      <Calendar />
+    </Main>
+  );
+};
+
+export default Schedule;
