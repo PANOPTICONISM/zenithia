7d023831143bfb482c652baae47695a229189bad
panopticonism <panopticonism@gmail.com>
2023-06-05 20:10:38 +0200

    Signup endpoint + Supabase Auth integration.

--- backend/api/signup.js
+++ backend/api/signup.js
@@ -1,20 +1,21 @@
 import supabase from "../supabaseClient.js";
 
-export const processUserDetails = async (req, res) => {
+export const signUp = async (req, res) => {
     const { params } = req;
 
     try {
-        const { data, error } = await supabase.auth.signInWithPassword({
+        const { data, error } = await supabase.auth.signUp({
             email: params.username,
             password: params.password,
           })
-
+              
         if (error) {
-          throw error;
+            res.status(401).json('401');
         }
         res.status(200).json(data);
+
     } catch (error) {
         console.error(error);
-        res.status(500).json({ error: 'An error occurred while fetching your data' });
+        res.status(500).json({ error: 'An error occurred while signing up' });
     }
 }
